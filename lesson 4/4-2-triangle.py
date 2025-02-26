ca0de399a43f6ad212bfe6f199be3ca7f26f5de6
rhage <139259164+rhagewannakiss@users.noreply.github.com>
2025-02-26 19:16:46 +0300

    Update 4-2-triangle.py

--- lesson 4/4-2-triangle.py
+++ lesson 4/4-2-triangle.py
@@ -8,8 +8,8 @@ dac = [8, 11, 7, 1, 0, 5, 12, 6]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
 
-def dec2bin(x):
-    return [(x >> 1) & 1 for i in range (7, -1, -1)]
+def dec2bin(num): 
+    return [int(num >> (7 - i)) & 1 for i in range (8)]
 
 try:
     period = float(input("Enter a period of sygnal: "))
@@ -19,11 +19,13 @@ try:
             GPIO.output(dac, dec2bin(x))
             x = x + 1
             sleep(period/512)
+            print("num:", x, "voltage: ", x/256.0 * 3.3)
         x = x - 1
         while(x >= 0):
             GPIO.output(dac, dec2bin(x))
             x = x - 1
             sleep(period/512)
+        else: break
 
 except Exception:
     print("Enter a number!")
